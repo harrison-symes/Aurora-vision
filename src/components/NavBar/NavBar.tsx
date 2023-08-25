9951289f5526e96c926956a92dc6f5f92ba16732
XEROINSIDE\harrison.symes <harrison.symes@xero.com>
2023-08-25 16:26:34 +1200

    Navbar improvements

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -8,31 +8,33 @@ const NavBar = () => {
     const [isActive, setIsActive] = React.useState<boolean>(false)
 
     return <div className="navbar__container">
-        <div className="navbar__left">
-            <Link to={"/"}>
-                <img src="/images/logo.png" className="navbar__logo" />
-            </Link>
-        </div>
-        <div className="navbar__right">
-
-            <div className="navbar__hamburger">
-                <Hamburger toggled={isActive} onToggle={() => setIsActive(state => !state)} />
+        <div className="navbar__inner">
+            <div className="navbar__left">
+                <Link to={"/"}>
+                    <img src="/images/logo.png" className="navbar__logo" />
+                </Link>
             </div>
-            <div className={cn("navbar__items", {
-                "is-active": isActive
-            })}>
-                <NavItem 
-                    href="/about-us"
-                    text="About us"
-                />
-                <NavItem 
-                    href="/our-work"
-                    text="Our work"
-                />
-                <NavItem 
-                    href="/contact"
-                    text="Get in touch"
-                />
+            <div className="navbar__right">
+
+                <div className="navbar__hamburger">
+                    <Hamburger toggled={isActive} onToggle={() => setIsActive(state => !state)} />
+                </div>
+                <div className={cn("navbar__items", {
+                    "is-active": isActive
+                })}>
+                    <NavItem 
+                        href="/about-us"
+                        text="About us"
+                    />
+                    <NavItem 
+                        href="/our-work"
+                        text="Our work"
+                    />
+                    <NavItem 
+                        href="/contact"
+                        text="Get in touch"
+                    />
+                </div>
             </div>
         </div>
     </div>
